08d10caa994996b99e37dc76367f45e37f481015
DOriginalSamxie <samadrufai@gmail.com>
2023-03-13 03:24:03 +0100

    template-product added

--- index.js
+++ index.js
@@ -27,6 +27,8 @@ const url = require('url')
 
 /////////////////////////////
 //SERVER
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8')
+const dataObj = JSON.parse(data)
 const server = http.createServer((req, res) => {
   console.log(req.url)
   const pathName = req.url
@@ -34,6 +36,11 @@ const server = http.createServer((req, res) => {
     res.end('This is the OVERVIEW')
   } else if (pathName === '/product') {
     res.end('This is the PRODUCT')
+  } else if (pathName === '/api') {
+    res.writeHead(202, {
+      'Content-type': 'application/json',
+    })
+    res.end(data)
   } else {
     res.writeHead(404, {
       'Content-type': 'text/html',
